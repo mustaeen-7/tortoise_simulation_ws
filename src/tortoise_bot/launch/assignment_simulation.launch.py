c11f870e4812491cf7da6892b506b378315eaff3
mustaeen-7 <mdmustaeenkhan777@gmail.com>
2025-10-23 17:15:24 +0530

    Update spawn launch files, remove test models, add new models and worlds

--- src/tortoise_bot/launch/assignment_simulation.launch.py
+++ src/tortoise_bot/launch/assignment_simulation.launch.py
@@ -1,44 +1,50 @@
 #!/usr/bin/env python3
 #
 # Copyright 2019 ROBOTIS CO., LTD.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+# Licensed under the Apache License, Version 2.0
 # Authors: Joep Tool
+#
 
 import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, SetEnvironmentVariable
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
-from launch.actions import SetEnvironmentVariable, ExecuteProcess
-
-
 
 def generate_launch_description():
     launch_file_dir = os.path.join(get_package_share_directory('tortoise_bot'), 'launch')
     pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
 
+    # Pose arguments
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
-    x_pose = LaunchConfiguration('x_pose', default='-2.0')
-    y_pose = LaunchConfiguration('y_pose', default='-0.5')
+    x_pose = LaunchConfiguration('x_pose', default='8.087546')
+    y_pose = LaunchConfiguration('y_pose', default='2.548081')
+    #z_pose = LaunchConfiguration('z_pose', default='0.008509')
+    roll = LaunchConfiguration('roll', default='0.000490')
+    pitch = LaunchConfiguration('pitch', default='0.006094')
+    yaw = LaunchConfiguration('yaw', default='-1.524284')
 
+    # Declare launch arguments
+    declare_x_cmd = DeclareLaunchArgument('x_pose', default_value='8.087546', description='X position')
+    declare_y_cmd = DeclareLaunchArgument('y_pose', default_value='2.548081', description='Y position')
+    #declare_z_cmd = DeclareLaunchArgument('z_pose', default_value='0.008509', description='Z position')
+    declare_roll_cmd = DeclareLaunchArgument('roll', default_value='0.000490', description='Roll')
+    declare_pitch_cmd = DeclareLaunchArgument('pitch', default_value='0.006094', description='Pitch')
+    declare_yaw_cmd = DeclareLaunchArgument('yaw', default_value='-1.524284', description='Yaw')
+
+    # Set TurtleBot3 model environment variable
+    env1 = SetEnvironmentVariable(
+        name='TURTLEBOT3_MODEL',
+        value='burger_cam'
+    )
+
+    # Load Gazebo world
     world = os.path.join(
         get_package_share_directory('tortoise_bot'),
         'worlds',
-        'turtlebot3_house.world'
+        'Nxtwave_full_world.world'
     )
 
     gzserver_cmd = IncludeLaunchDescription(
@@ -67,20 +73,26 @@ def generate_launch_description():
         ),
         launch_arguments={
             'x_pose': x_pose,
-            'y_pose': y_pose
+            'y_pose': y_pose,
+            #'z_pose': z_pose,
+            'roll': roll,
+            'pitch': pitch,
+            'yaw': yaw
         }.items()
     )
-    
-    env1 = SetEnvironmentVariable(
-    name='TURTLEBOT3_MODEL',
-    value='burger_cam'
-)
 
-
-
+    # LaunchDescription
     ld = LaunchDescription()
 
-    # Add the commands to the launch description
+    # Declare arguments first
+    ld.add_action(declare_x_cmd)
+    ld.add_action(declare_y_cmd)
+    #ld.add_action(declare_z_cmd)
+    ld.add_action(declare_roll_cmd)
+    ld.add_action(declare_pitch_cmd)
+    ld.add_action(declare_yaw_cmd)
+
+    # Add environment and nodes
     ld.add_action(env1)
     ld.add_action(gzserver_cmd)
     ld.add_action(gzclient_cmd)
@@ -88,3 +100,4 @@ def generate_launch_description():
     ld.add_action(spawn_turtlebot_cmd)
 
     return ld
+
--- src/tortoise_bot/launch/spawn_turtlebot3.launch.py
+++ src/tortoise_bot/launch/spawn_turtlebot3.launch.py
@@ -1,17 +1,3 @@
-# Copyright 2019 Open Source Robotics Foundation, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import os
 
 from ament_index_python.packages import get_package_share_directory
@@ -19,53 +5,42 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
-from launch.actions import SetEnvironmentVariable, ExecuteProcess
-
 
 def generate_launch_description():
-    # Get the urdf file
+    # Get the model path from environment variable
     TURTLEBOT3_MODEL = os.environ['TURTLEBOT3_MODEL']
     model_folder = 'turtlebot3_' + TURTLEBOT3_MODEL
     urdf_path = os.path.join(
         get_package_share_directory('tortoise_bot'),
         'models',
         model_folder,
-        'model.sdf'
+        'model.sdf'  # ✅ make sure this file actually exists
     )
 
-    # Launch configuration variables specific to simulation
-    x_pose = LaunchConfiguration('x_pose', default='0.0')
-    y_pose = LaunchConfiguration('y_pose', default='0.0')
+    # Launch configuration variables
+    x_pose = LaunchConfiguration('x_pose', default='8.087546')
+    y_pose = LaunchConfiguration('y_pose', default='2.548081')
+    roll  = LaunchConfiguration('roll',  default='0.000490')
+    pitch = LaunchConfiguration('pitch', default='0.006094')
+    yaw   = LaunchConfiguration('yaw',   default='-1.524284')
 
-    # Declare the launch arguments
-    declare_x_position_cmd = DeclareLaunchArgument(
-        'x_pose', default_value='0.0',
-        description='Specify namespace of the robot')
-
-    declare_y_position_cmd = DeclareLaunchArgument(
-        'y_pose', default_value='0.0',
-        description='Specify namespace of the robot')
-
+    # Node to spawn entity in Gazebo
     start_gazebo_ros_spawner_cmd = Node(
         package='gazebo_ros',
         executable='spawn_entity.py',
         arguments=[
-            '-entity', TURTLEBOT3_MODEL,
-            '-file', urdf_path,
+            '-entity', 'turtlebot3',
+            '-file', urdf_path,       # ✅ use the actual URDF/SDF path here
             '-x', x_pose,
             '-y', y_pose,
-            '-z', '0.01'
+            '-R', roll,
+            '-P', pitch,
+            '-Y', yaw
         ],
-        output='screen',
+        output='screen'
     )
 
     ld = LaunchDescription()
-
-    # Declare the launch options
-    ld.add_action(declare_x_position_cmd)
-    ld.add_action(declare_y_position_cmd)
-
-    # Add any conditioned actions
     ld.add_action(start_gazebo_ros_spawner_cmd)
-
     return ld
+
